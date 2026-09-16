58649da1752e5b8ae9bf3cc76276a4b18812edd5
周珩 <yyi097832@gmail.com>
2026-09-16 16:44:16 +0800

    Delete my-space-github-clean directory

--- my-space-github-clean/middleware/auth.js
+++
@@ -1,8 +0,0 @@
-// 登录验证中间件
-module.exports = function auth(req, res, next) {
-  if (req.session && req.session.isAdmin) {
-    return next();
-  }
-  req.session.returnTo = req.originalUrl;
-  res.redirect('/admin/login');
-};
